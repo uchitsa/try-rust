7929056d0c7ec72fe14beb844c4bedda7b679722
uchitsa <uchitsa@ya.ru>
2022-12-12 02:40:03 +0300

    Create primitives.rs

---
+++ primitives.rs
@@ -0,0 +1,14 @@
+fn main() {
+  let logic: bool = true;
+
+  let a_float: f64 = 1.0;
+  let a_int        = 5i32;
+
+  let def_float = 1.0;
+  let def_int   = 7;
+
+  let mut mutable = 42;
+  mutable = 7;
+
+  let mutable = true;
+}
